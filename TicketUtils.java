77f971bf48c219f076ab2a06e7a9714f2bcab131
miami01741 <08maime@gmail.com>
2025-04-30 23:31:05 +0200

    Add files via upload

---
+++ TicketUtils.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class TicketUtils {
+    // Selection Sort by price
+    public static void selectionSortByPrice(ArrayList<Ticket> tickets) {
+        for (int i = 0; i < tickets.size() - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < tickets.size(); j++) {
+                if (tickets.get(j).getPrice() < tickets.get(minIndex).getPrice()) {
+                    minIndex = j;
+                }
+            }
+            Ticket temp = tickets.get(i);
+            tickets.set(i, tickets.get(minIndex));
+            tickets.set(minIndex, temp);
+        }
+    }
+
+    // Insertion Sort by holder name
+    public static void insertionSortByName(ArrayList<Ticket> tickets) {
+        for (int i = 1; i < tickets.size(); i++) {
+            Ticket key = tickets.get(i);
+            int j = i - 1;
+            while (j >= 0 && tickets.get(j).getHolderName().compareToIgnoreCase(key.getHolderName()) > 0) {
+                tickets.set(j + 1, tickets.get(j));
+                j--;
+            }
+            tickets.set(j + 1, key);
+        }
+    }
+
+    // Linear Search by holder name
+    public static int linearSearchByName(ArrayList<Ticket> tickets, String name) {
+        for (int i = 0; i < tickets.size(); i++) {
+            if (tickets.get(i).getHolderName().equalsIgnoreCase(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Binary Search by price (requires sorted list)
+    public static int binarySearchByPrice(ArrayList<Ticket> tickets, double price) {
+        int low = 0, high = tickets.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            double currentPrice = tickets.get(mid).getPrice();
+            
+            if (currentPrice == price) return mid;
+            else if (currentPrice < price) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
